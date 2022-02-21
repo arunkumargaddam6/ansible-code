7b2d18f14a8be9843f7c207f0f1b9dc32ba1a11a
EC2 Default User <ec2-user@ip-172-31-40-87.ap-southeast-2.compute.internal>
2022-02-21 11:34:49 +0000

    new commit

---
+++ shellscript/practice/file.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# echo "enter the file name"
+# read file
+# if [ -f "$file" ]
+# then
+#     echo "please enter the text"
+#     read text
+#     echo "$text" >> $file
+# else
+#     echo "file does not exist"    
+# fi
+
+echo "enter the file name"
+read file
+if [ -f "$file" ]
+then
+    while IFS= read -r line
+    do
+        echo $line
+    done < $file
+else    
+    echo "file does not exist"
+fi
