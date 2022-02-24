d97f4d7702af75757431174e66eed2979b888dd2
arunkumargaddam6 <arunkumargaddam6@gmail.com>
2022-02-24 02:05:22 +0000

    new

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,7 +1,7 @@
-resource "aws_instance" "testinstance" {
-  ami           = "ami-0bc96915949503483 "
+resource "aws_instance" "test1instance" {
+  ami           = "ami-0de587b30a36448ef"
   instance_type = "t2.micro"
   tags = {
-    "Name" = "Myinstance"
+    "Name" = "Minstance"
   }
 }
