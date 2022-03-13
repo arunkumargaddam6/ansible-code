2a5da88697e29d8bbe6f9854acf8e41548abfdbd
EC2 Default User <ec2-user@ip-172-31-33-221.ap-southeast-2.compute.internal>
2022-03-13 23:39:04 +0000

    new

---
+++ practice/log.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "enter the file name"
+read logfile
+maxsize='100'
+timestamp=`date +%d%m`
+
+if [ -f $logfile ]
+then
+    echo "file exist and checking its size"
+    size=`du -b $logfile | awk -F " " '{print $1}'`
+    if [ $size -gt $maxsize ]
+    then
+        echo "rotate the file"
+        mv $logfile $logfile.$timestamp
+        touch $logfile
+    else
+        echo "file size not exceeded"
+    fi
+else 
+    echo "file does not exist"
+fi
